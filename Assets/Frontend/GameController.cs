27a76b361322a5a9f70b3d08f93c9b85776739cb
Derrick Sanchez <djs225@njit.edu>
2026-08-05 12:00:30 -0400

    Added Key/Door Interaction, bug with Spawning

--- Assets/Frontend/GameController.cs
+++ Assets/Frontend/GameController.cs
@@ -1,7 +1,14 @@
 using UnityEngine;
 
+
+
 public class GameController : MonoBehaviour
 {
+
+    public HUDManager HUDManager;
+    public GameObject Door;
+    public GameObject Key;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
