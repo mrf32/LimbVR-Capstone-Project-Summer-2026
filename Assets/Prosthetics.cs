3a7f6e3ebf55a69eed549e554113fe3891cb902f
XT64 <xt64@njit.edu>
2026-08-05 20:23:13 -0400

    Update Prosthetics.cs

--- Assets/Prosthetics.cs
+++ Assets/Prosthetics.cs
@@ -115,8 +115,7 @@ public class Prosthetics : MonoBehaviour
         target.transform.Translate(0, translation_h, 0);
 
         // Rotate around our y-axis
-        visualMeshChild.Rotate(0, -rotation, 0);
-
+        visualMeshChild.Rotate(0, 0, -rotation);
 
 
         // Debug.Log(Score);
@@ -154,9 +153,19 @@ public class Prosthetics : MonoBehaviour
             graspStatus = false;
         }
 
-        if (isMoving == 1 & Object.stayInGraspDomain == 1) 
+
+        if (isMoving == 1 && Object.stayInGraspDomain == 1 && target != null) 
         {
-            robot.transform.position = Vector3.MoveTowards(robot.transform.position, target.transform.position + new Vector3(-0.01f,0.060f,0.2f), speed);
+
+            Transform gripAnchor = target.transform.Find("Grip_Anchor");
+            
+            if(gripAnchor != null){
+                robot.transform.position = Vector3.MoveTowards(robot.transform.position, gripAnchor.position, speed);
+            }
+            else{
+                robot.transform.position = Vector3.MoveTowards(robot.transform.position, target.transform.position, speed);
+            }
+
         }
 
         if (Door.InTrashCan & vlxFloat4 < 60 & gameTimer > 0f)
@@ -183,6 +192,7 @@ public class Prosthetics : MonoBehaviour
 
 
     }
+
     void CheckLoggingInput()
     {
         foreach (KeyCode key in loggingKeys)
